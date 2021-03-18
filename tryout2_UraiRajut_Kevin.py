f754021263848a0823b7a0063b1a5d052d00d9de
maxevin <80762703+maxevin@users.noreply.github.com>
2021-03-18 21:24:32 +0700

    Add files via upload

---
+++ tryout2_UraiRajut_Kevin.py
@@ -0,0 +1,15 @@
+def urai(a):
+    a = 'Code'
+    b = len(a)
+    for i in range(b):
+        for j in range(i+1):
+            print(a[j], end='')
+        print
+    return
+
+# def rajut():
+
+
+print(urai('Code'))
+print(urai('Python'))
+
